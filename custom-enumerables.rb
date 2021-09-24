841390c7d232aa39e2a0881b8811ba29de6a016e
Michael Chong <mikec0920@gmail.com>
2021-09-24 12:51:44 -0700

    Added default return for my_each

--- custom-enumerables.rb
+++ custom-enumerables.rb
@@ -4,6 +4,9 @@ module Enumerable
       for item in self
         yield item
       end
+      self
+    else
+      Enumerator.new(self)
     end
   end
 
@@ -16,14 +19,28 @@ module Enumerable
       end
     end
   end
+
+  def my_select
+    if block_given?
+
+      self.my_each do |item|
+        
+      end
+    end
+  end
 end
 
-# puts "my_each vs. each"
-# numbers = [1, 2, 3, 4, 5]
-# numbers.my_each  { |item| puts item }
-# numbers.each  { |item| puts item }
-
-puts "my_each_with_index vs. each_with_index"
+puts "my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
-numbers.my_each_with_index  { |item, idx| puts "#{idx}: #{item}" }
-numbers.each_with_index  { |item, idx| puts "#{idx}: #{item}" }
+numbers.my_each { |item| puts item }
+numbers.each { |item| puts item }
+
+# puts "my_each_with_index vs. each_with_index"
+# numbers = [1, 2, 3, 4, 5]
+# numbers.my_each_with_index  { |item, idx| puts "#{idx}: #{item}" }
+# numbers.each_with_index  { |item, idx| puts "#{idx}: #{item}" }
+
+# puts "my_select vs. select"
+# numbers = [1, 2, 3, 4, 5]
+# puts numbers.my_select  { |item| item > 3 }
+# puts numbers.select  { |item| item > 3 }
