5914686801b850154d0c3e4240d191a30203fd0a
Michael Chong <mikec0920@gmail.com>
2021-09-24 11:51:27 -0700

    Implemented my_each_with_index

--- custom-enumerables.rb
+++ custom-enumerables.rb
@@ -9,19 +9,21 @@ module Enumerable
 
   def my_each_with_index
     if block_given?
-      for item in self
-        yield item
+      current_index = 0
+      while current_index < self.length
+        yield self[current_index], current_index
+        current_index += 1
       end
     end
   end
 end
 
-puts "my_each vs. each"
-numbers = [1, 2, 3, 4, 5]
-numbers.my_each  { |item| puts item }
-numbers.each  { |item| puts item }
-
-# puts "my_each_with_index vs. each_with_index"
+# puts "my_each vs. each"
 # numbers = [1, 2, 3, 4, 5]
-# numbers.my_each_with_index  { |item, idx| puts "#{idx}: #{item}" }
-# numbers.each_with_index  { |item, idx| puts "#{idx}: #{item}" }
+# numbers.my_each  { |item| puts item }
+# numbers.each  { |item| puts item }
+
+puts "my_each_with_index vs. each_with_index"
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each_with_index  { |item, idx| puts "#{idx}: #{item}" }
+numbers.each_with_index  { |item, idx| puts "#{idx}: #{item}" }
